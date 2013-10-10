9828f84a39b501794d21e0ec72a2828cea84c097
Richard Milne <rmilne@whelk.(none)>
2013-10-10 15:15:01 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='jaro_winkler',
+    version='1.0',
+    description='Original, standard and customisable versions of the Jaro-Winkler functions.',
+    author='Richard Milne',
+    author_email='richmilne@hotmail.com',
+    url='https://github.com/richmilne/JaroWinkler.git',
+    packages=['jaro'],
+    platforms=['any'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Indexing',
+    ],
+     )
+
