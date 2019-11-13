a920f720db1b03bd376aecba78989005dd7dc036
Richard Milne <richmilne@hotmail.com>
2019-11-13 10:38:15 +0100

    Ensure pypi recognises our markdown readme

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
-# from distutils.core import setup
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / 'README.md').read_text()
+
+# TODO: Package / expose functionality on the command line, too.
 
 setup(
     name='jaro_winkler',
     version='1.9.0',
     description='Original, standard and customisable versions of the Jaro-Winkler functions.',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='Richard Milne',
     author_email='richmilne@hotmail.com',
     url='https://github.com/richmilne/JaroWinkler.git',
